5bea4b60987d428ebb3d262f9582fd0611416b99
mxmldesign <nesignsprivate@gmail.com>
2025-06-20 17:54:49 +0100

    map

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import styles from "./Button.module.css"
 
-export default function Button( {children} ) {
+export default function Button({ children, onClick }) {
   return (
-    <button className={styles.button} type="button">
+    <button className={styles.button} type="button" onClick={onClick}>
       <span className={styles.buttonInner}>
-        { children }
+        {children}
       </span>
     </button>
   )
--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -3,7 +3,7 @@ import styles from './Display.module.css'
 export default function Display({ children }) {
   return (
     <div className={styles.display}>
-      { children }
+      {children}
     </div>
   )
 }

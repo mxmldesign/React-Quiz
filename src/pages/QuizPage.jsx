8bf8af47325346d0851fbb2305ef0b4d3241db58
mxmldesign <nesignsprivate@gmail.com>
2025-06-20 18:23:13 +0100

    Proceeding_to_the_ResultPage_1
    filter

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,10 +1,15 @@
+import { useEffect, useState } from "react";
 import Button from "../components/Button/Button"
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
+import { ROUTES } from "../const";
+import { useNavigate } from "react-router-dom";
 
 export default function QuizPage() {
   const [quizIndex, setQuizIndex] = useState(0);
   const [answerLogs, setAnswerLogs] = useState([]);
+  const navigation = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length
   
   const handleClick = (clickedIndex) => {
     if (clickedIndex === quizData[quizIndex].answerIndex){
@@ -14,6 +19,20 @@ export default function QuizPage() {
     }
     setQuizIndex((prev) => prev +1);
   };
+
+  useEffect(()=> {
+    if(answerLogs.length === MAX_QUIZ_LEN){
+      const correctNum = answerLogs.filter((answer) => {
+        return answer === true
+      })
+      navigation(ROUTES.RESULT, {
+        state: {
+          maxQuizLen: MAX_QUIZ_LEN, 
+          correctNum: correctNum
+        }
+      });
+    }
+  },[answerLogs]);
 
   return (
     <>

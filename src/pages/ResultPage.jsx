7fab40656d45c707c9b5d19cf965a482bfcab036
mxmldesign <nesignsprivate@gmail.com>
2025-07-18 15:29:54 +0100

    Basic functions of result page

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,19 @@
+import { Link, useLocation } from "react-router-dom";
+import { ROUTES } from "../const";
+
+
 export default function ResultPage() {
+  const location = useLocation();
+  const maxQuizLen = location.state.maxQuizLen
+  const correctNumLen = location.state.correctNumLen
+
   return (
-    <div>ResultPage</div>
+    <>
+      <h1>Result</h1>
+      <p>You answered...</p>
+      <p>{`${correctNumLen} questions correct out of ${maxQuizLen} questions!`}</p>
+      <br />
+      <Link to={ROUTES.QUIZ}>Try again</Link>
+    </>
   )
 }
